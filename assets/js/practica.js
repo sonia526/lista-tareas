27d07dc8deda2e1dac01ae906ccb79802ea60d82
sonia526 <zamirastars3@gmail.com>
2017-07-26 18:24:19 -0500

    modificado con los array y html

--- assets/js/practica.js
+++ assets/js/practica.js
@@ -1,14 +1,86 @@
+var tareas=[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }]
+
+  var html = "";
+   for (var i=0; i<tareas.length;i++)
+   {
+   	var dato_1 = tareas[i];
+   	//alert (dato_1);
+   	var s = "<li>" +  dato_1.title + "</li>"
+   	html += s;
+   }
+  document.getElementById( 'lista' ).innerHTML = html;
+
 (function(){
 	// Variables
  var lista = document.getElementById("lista"),
 		tareaInput = document.getElementById("tareaInput"),
 		btnNuevaTarea = document.getElementById("btn-agregar");
-    
+
 	// Funciones
 	var agregarTarea = function(){
 		var tarea = tareaInput.value,
 			nuevaTarea = document.createElement("li"),
-			enlace = document.createElement("a"),
+			//enlace = document.createElement("a"),
 			contenido = document.createTextNode(tarea);
 
 		if (tarea === "") {
@@ -18,11 +90,11 @@
 		}
 
 		// Agregamos el contenido al enlace
-		enlace.appendChild(contenido);
+		nuevaTarea.appendChild(contenido);
 		// Le establecemos un atributo href
-		enlace.setAttribute("href", "#");
+		//enlace.setAttribute("href", "#");
 		// Agrergamos el enlace (a) a la nueva tarea (li)
-		nuevaTarea.appendChild(enlace);
+		nuevaTarea.appendChild(contenido);
 		// Agregamos la nueva tarea a la lista
 		lista.appendChild(nuevaTarea);
 
